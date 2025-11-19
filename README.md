360e0a2c3263632219ea69dd826894cf6dd8a74d
kat-ausmus <katausmus@gmail.com>
2025-11-19 10:57:05 -0600

    WIP: Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# UCR Web
+powered by React/Typescript/Vite
+
+A web app to manage UCR data via ucr API (in another repo)
+
+
---
+++ src/components/FlexRow.tsx
@@ -0,0 +1,15 @@
+import type {UcrRowDataType} from "../types/ucrResponseType.ts";
+
+const FlexRow =  ({data}: {data: UcrRowDataType}  ) => {
+    return (
+        <div className="row-view">
+            <p className="text-xl text-center"> {data.id} </p>
+            <p> {data.ori} </p>
+            <p> {data.state_name} </p>
+            <p> {data.data_year} </p>
+            <p> {data.actual_count} </p>
+            <p> {data.unfounded_count} </p>
+        </div>
+    )
+}
+export default FlexRow
